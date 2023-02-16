42b0dd22dff7b9193ab2c41e01a150a3acb41881
bahati <bulozealfani@gmail.com>
2023-02-16 09:55:04 -0600

    added description

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.title("Buloze Alfani")
+content = """
+Hi, I'm Buloze, welcome to my project site, on here are some of the web apps I've 
+developed so far with many more to come as I continue learning to
+improve on my Python skills, all these applications are projects 
+I build in my journey to learning and improving on skills as a programmer, 
+I had fun building these projects because a lot of them really challenged me
+on my skills as a programmer. I hope you enjoy.
+"""
+st.info(content)
